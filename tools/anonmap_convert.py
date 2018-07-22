16c6d0e135d09ff6db45d41a6141e66357619e0a
johannes walcher <johannes.walcher@stusta.de>
2018-07-22 23:42:21 +0200

    Updated anonmap_convert to actually work

--- tools/anonmap_convert.py
+++ tools/anonmap_convert.py
@@ -1,24 +1,46 @@
-import sys
+#!/usr/bin/env python3
+
+"""
+Convert an autofs5 anonmapping to a mammutfs anonmapping preserving the suffixes
+"""
+
+import os
 import re
+import sys
 
-class converter:
-    def __init__(self):
-        pass
+def convert(infname, outfname):
+    """
+    Read the old anonmap generate the new one
+    This method will always create a new out-mapping
+    And will only read the normal anonymous files - not public files
+    that are also required in mammutfs
+    """
+    converted = 0
+    failed = 0
+    with open(infname, 'r') as infile:
+        with open(outfname, 'w+') as outfile:
+            for line in infile:
+                match = re.match(r"^a_(.*)_(\w\w\w) --bind :(.*)", line)
+                success = False
+                for candidate in match.group(3).split(":"):
+                    candidate = candidate.strip()
+                    if os.path.isdir(candidate):
+                        newline = "a_{}_{}:{}\n".format(
+                            match.group(1),
+                            match.group(2),
+                            candidate)
+                        converted += 1
+                        outfile.write(newline)
+                        success = True
+                        break
+                if not success:
+                    print("Could not find origin for anonmap line: ", line)
+                    failed += 1
 
-    def convert(self, infname, outfname):
-        with open(infname, 'r') as infile:
-            with open(outfname, 'w+') as outfile:
-                for line in infile:
-                    match = re.match(r"^a_(.*)_(\w\w\w) .*:(.*)", line)
-                    newline = "a_{}_{}:{}\n".format(match.group(1), 
-                            match.group(2), match.group(3))
-                    outfile.write(newline)
-
+    print("Converted: %s; Not found: %s "%(converted, failed))
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: " + sys.argv[0] + " [old anon map] [new anon map]")
         exit(1)
 
-    cv = converter()
-    cv.convert(sys.argv[1], sys.argv[2])
-
+    convert(sys.argv[1], sys.argv[2])
